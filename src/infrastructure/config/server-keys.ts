c297534cefdd729ae226bdcb59d052659c387107
MaxShady <max.shady1611@gmail.com>
2022-06-21 17:47:52 +0430

    Update sever keys

--- src/infrastructure/config/server-keys.ts
+++ src/infrastructure/config/server-keys.ts
@@ -4,5 +4,5 @@ export class ServerKeys {
     public static readonly PORT = get("PORT").required().asPortNumber();
     public static readonly SALT_ROUNDS = get("SALT_ROUNDS").required().asInt();
     public static readonly JWT_SECRET = get("JWT_SECRET").required().asString();
-    public static JWT_EXPIRES_IN = get("JWT_EXPIRES_IN").required().asString();
+    public static readonly JWT_EXPIRES_IN = get("JWT_EXPIRES_IN").required().asString();
 }
