89ef572bad2febd073c074601920759b99c58929
MaxShady <max.shady1611@gmail.com>
2022-06-23 15:57:34 +0430

    Use nest logger

--- src/application/server.ts
+++ src/application/server.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app.module";
 import { ServerKeys } from "../infrastructure/config/server-keys";
+import { Logger } from "@nestjs/common";
 
 export class Server {
 
+    private readonly logger = new Logger();
     private readonly PORT = ServerKeys.PORT;
 
     async run() {
@@ -15,7 +17,7 @@ export class Server {
     }
 
     private log() {
-        console.log(`Listening on port ${this.PORT}`)
+        this.logger.log(`Listening on port ${this.PORT}`);
     }
 
     public static build(): Server {
